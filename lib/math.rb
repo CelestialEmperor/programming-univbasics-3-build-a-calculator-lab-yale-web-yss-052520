9975578c9c5a7ba6031ec3f691adb148b5dd1aeb
Jack Ma <mapeiyao@gmail.com>
2020-05-25 09:56:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,20 +5,27 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
-
+  output = num1 - num2
+  puts output
+  return output
 end
 
 def division(num1, num2)
-
+  output = num1 / num2
+  puts output
+  return output
 end
 
 def multiplication(num1, num2)
-
+  output = num1 * num2
+  puts output
+  return output
 end
 
 def modulo(num1, num2)
-
+  output = num1 % num2
+  puts output
+  return output
 end
 
 def square_root(num)
